f4bcabd25e78368db639c446a8981ece2bfcd8e8
umitotuki <umitukiuk@gmail.com>
2024-05-11 07:47:40 +0000

    update

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-$(function(){
-  $('.box1').slideDown();
+$(function (){
+  $('.box1').slideDown(5000,function (){
+    $('.box1').css({
+      'background-color': '#ffd900',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp(5000);
+  });
 });
